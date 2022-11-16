f0ddc88be2ef2d403bc770ce85e8a013adee6d57
DaniilVenikov <daniilvenikoff@gmail.com>
2022-11-16 20:21:26 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,54 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class PhoneBookTest {
-    
+
+    PhoneBook phoneBook = new PhoneBook();
+
+    private static long suiteStartTime;
+    private long testStartTime;
+
+    @BeforeAll
+    public static void started(){
+        System.out.println("tests started");
+        suiteStartTime = System.nanoTime();
+    }
+
+    @BeforeEach
+    public void init(){
+        System.out.println("test started");
+        testStartTime = System.nanoTime();
+    }
+
+    @AfterEach
+    public void finished(){
+        System.out.println("test completed" + "\n"
+                + "time test: " + (System.nanoTime() - testStartTime));
+    }
+
+    @AfterAll
+    public static void finishedAll(){
+        System.out.println("tests completed" + "\n"
+                + "time tests: " + (System.nanoTime() - suiteStartTime));
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("sourceForAddTest")
+    void addTest(String name, String phoneNumber, int expected){
+        assertThat(phoneBook.add(name, phoneNumber), is(equalTo(expected)));
+    }
+
+    static Stream<Arguments> sourceForAddTest(){
+        return Stream.of(Arguments.of("Sasha", "89203297536", 1),
+                Arguments.of("Vasya","89193459090", 2),
+                Arguments.of("Sasha", "89191237956", 2));
+    }
 }
